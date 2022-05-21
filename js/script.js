628f5dd712538c745cd854f769e17e0c4868896b
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-05-21 14:09:24 -0300

    Smooth scrolling animation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
+// MOBILE NAV BUTTON CLICK EVENT
 let mobileButtonEl = document.querySelector(".mobile-nav-button");
 
 mobileButtonEl.addEventListener("click", () => {
   let headerEl = document.querySelector("header");
   headerEl.classList.toggle("nav-open");
+});
+
+// SMOOTH SCROLLING ANIMATION
+let links = document.querySelectorAll("a:link");
+
+links.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    let href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      let sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+      if (link.classList.contains("main-nav-link"))
+        document.querySelector("header").classList.remove("nav-open");
+    }
+  });
 });
